e3f6bde9dee1dcd66eba0b543d165e18d4e0033d
VasyaStelmah <stelmah.v.m@gmail.com>
2021-01-15 21:18:58 +0300

    Add .env file

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -14,14 +14,14 @@ const userRoutes = require("./routes/user");
 
 const app = express();
 
-// mongoose
-//   .connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(() => console.log("MongoDB connected..."))
-//   .catch((error) => console.log(error));
-// mongoose.set("useCreateIndex", true);
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected..."))
+  .catch((error) => console.log(error));
+mongoose.set("useCreateIndex", true);
 
 app.use(express.static("public"));
 app.use(
